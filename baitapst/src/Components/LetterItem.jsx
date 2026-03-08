e11ab1bc4b73c4bdb8d44ad0bef7cdf503196593
nguyenthanhnhat4350 <nguyenthanhnhat4350@gmail.com>
2026-03-09 01:00:00 +0700

    dua code baitapst ra ngoai le

---
+++ baitapst/src/Components/LetterItem.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+
+function LetterItem({ letter }) {
+    useEffect(() => {
+        console.log("LetterItem mounted:", letter.id);
+
+        return () => {
+            console.log("LetterItem mounted:", letter.id);
+        };
+    }, []);
+
+  return (
+      <div
+          type={{
+          border: "1px solid #ccc",
+          padding: "10px",
+          marginBottom: "10px",
+        }}
+        >
+        {letter.content}
+        </div>
+  );
+}
+
+export default LetterItem;
